1ede4334965cef1daa691dade552d43f744e62bc
unknown <vahid.firoozian@gmail.com>
2023-01-28 15:58:24 +0330

    add Timer component and run timer function in

---
+++ src/components/Timer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <button @click="timer()">click</button>
+        {{second}}
+    </div>
+</template>
+
+<script setup>
+import {onMounted, onUpdated, ref, watch} from "vue"
+    const second = ref(5);
+    console.log("salam")
+    
+    const timer = () => {
+        console.log("baz dobare")
+        if(second.value > 0){
+                console.log(second.value)
+                setTimeout(() => {
+                    second.value--
+                    console.log(second.value)
+                    timer()}
+                    ,1000)   
+                    
+                    
+                }
+    }
+    timer()
+    
+
+
+   
+</script>
